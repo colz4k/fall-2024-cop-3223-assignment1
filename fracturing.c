952417571a93cbafa50007c2aa16227733502d02
colz4k <zrycolman@gmail.com>
2024-09-09 00:50:18 +0000

    it works now

--- fracturing.c
+++ fracturing.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <math.h>
 
-#define PI 3.14159  // Define PI
+#define PI 3.14159
 
 // Function declarations
 double calculateDistance(double x1, double y1, double x2, double y2);
@@ -14,21 +14,21 @@ int main(int argc, char **argv) {
     double x1, y1, x2, y2;
     double distance, perimeter, area, width, height;
 
-    // Input points for distance, perimeter, area, width, height calculations
+    // Input points for distance, perimeter, area, width, height
     printf("Enter coordinates for Point #1 (x1 y1): ");
     scanf("%lf %lf", &x1, &y1);
 
     printf("Enter coordinates for Point #2 (x2 y2): ");
     scanf("%lf %lf", &x2, &y2);
 
-    // Call respective functions and store results
+    // Call functions and store results
     distance = calculateDistance(x1, y1, x2, y2);
     perimeter = calculatePerimeter(x1, y1, x2, y2);
     area = calculateArea(x1, y1, x2, y2);
     width = calculateWidth(x1, y1, x2, y2);
     height = calculateHeight(x1, y1, x2, y2);
 
-    // Display the calculated values
+    // Display the values
     printf("Distance between points: %lf\n", distance);
     printf("Perimeter: %lf\n", perimeter);
     printf("Area: %lf\n", area);
@@ -38,11 +38,11 @@ int main(int argc, char **argv) {
     return 0;
 }
 
-// Function to calculate distance between two points
+// Calculate distance between two points
 double calculateDistance(double x1, double y1, double x2, double y2) {
     double distance = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
 
-    // Output the points and calculated distance
+    
     printf("Point #1 entered: x1 = %lf; y1 = %lf\n", x1, y1);
     printf("Point #2 entered: x2 = %lf; y2 = %lf\n", x2, y2);
     printf("The distance between the two points is %lf\n", distance);
@@ -50,40 +50,40 @@ double calculateDistance(double x1, double y1, double x2, double y2) {
     return distance;
 }
 
-// Function to calculate perimeter based on distance
+// Calculate perimeter based on distance
 double calculatePerimeter(double x1, double y1, double x2, double y2) {
     double distance = calculateDistance(x1, y1, x2, y2);
-    double perimeter = 2 * PI * distance;  // Hypothetical perimeter using PI
+    double perimeter = 2 * PI * distance;
 
     printf("The perimeter of the city encompassed by your request is %lf\n", perimeter);
 
     return perimeter;
 }
 
-// Function to calculate area
+// Calculate area
 double calculateArea(double x1, double y1, double x2, double y2) {
     double distance = calculateDistance(x1, y1, x2, y2);
-    double area = PI * pow(distance, 2); // Hypothetical area calculation using PI
+    double area = PI * pow(distance, 2);
 
     printf("The area of the city encompassed by your request is %lf\n", area);
 
     return area;
 }
 
-// Function to calculate width
+// Calculate width
 double calculateWidth(double x1, double y1, double x2, double y2) {
     double distance = calculateDistance(x1, y1, x2, y2);
-    double width = distance / 2;  // Hypothetical width calculation
+    double width = distance / 2;
 
     printf("The width of the city encompassed by your request is %lf\n", width);
 
     return width;
 }
 
-// Function to calculate height
+// Calculate height
 double calculateHeight(double x1, double y1, double x2, double y2) {
     double distance = calculateDistance(x1, y1, x2, y2);
-    double height = distance * 1.5;  // Hypothetical height calculation
+    double height = distance * 1.5;
 
     printf("The height of the city encompassed by your request is %lf\n", height);
 
